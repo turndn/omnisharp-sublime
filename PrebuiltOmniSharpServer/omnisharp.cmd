turndn/omnisharp-sublime

commit 8e374d215036865d184519f66fc577b1805269ab

Update to beta3 bits, close #120

PrebuiltOmniSharpServer/omnisharp.cmd
@@ -1,2 +1,2 @@
 
-@"klr.exe" --appbase "%~dp0approot\packages\OmniSharp\1.0.0\root" Microsoft.Framework.ApplicationHost omnisharp %*
+@"%~dp0approot\packages\kre-mono.1.0.0-beta3\bin\klr.exe" --appbase "%~dp0approot\packages\OmniSharp\1.0.0\root" Microsoft.Framework.ApplicationHost omnisharp %*

commit 07e36017cda27faa56e7149be1fc0322555f0232

Fix klr.exe path again
Applying PR #113 again (broken by 8e374d215036865d184519f66fc577b1805269ab)

PrebuiltOmniSharpServer/omnisharp.cmd
@@ -1,2 +1,2 @@
 
-@"%~dp0approot\packages\kre-mono.1.0.0-beta3\bin\klr.exe" --appbase "%~dp0approot\packages\OmniSharp\1.0.0\root" Microsoft.Framework.ApplicationHost omnisharp %*
+@"klr.exe" --appbase "%~dp0approot\packages\OmniSharp\1.0.0\root" Microsoft.Framework.ApplicationHost omnisharp %*